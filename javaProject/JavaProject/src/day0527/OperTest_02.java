79e6e802d26b94833eca5e767f9fdb2900a69f22
hhh <gks0405@koreatech.ac.kr>
2020-05-27 10:14:59 +0900

    0527_2

---
+++ javaProject/JavaProject/src/day0527/OperTest_02.java
@@ -0,0 +1,10 @@
+package day0527;
+
+public class OperTest_02 {
+	public static void main(String[] args) {
+		//비교연산자 : < > >= <=
+		//논리연산자 : &&(and),||(or)
+		int a=5,b=3,c=5;
+		System.out.println(a>b);
+	}
+}
