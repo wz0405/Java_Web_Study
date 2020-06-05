634a6e4acf7dce5cbbca03fd036dd8bb9e055c6a
hhh <gks0405@koreatech.ac.kr>
2020-06-05 09:41:50 +0900

    0605

---
+++ javaProject/JavaProject/src/java6/test.java
@@ -0,0 +1,18 @@
+package java6;
+class A{
+	public void test(B b) {
+		b.test1();
+	}
+}
+class B{
+	public void test1() {
+		System.out.println("hi");
+	}
+}
+public class test {
+	public static void main(String[] args) {
+		A a = new A();
+		B b = new B();
+		a.test(b);
+	}
+}
