438e25d69d83cc95ed2b64e6deb5d2cc035e1440
hhh <gks0405@koreatech.ac.kr>
2020-06-11 15:38:41 +0900

    0611_js_event

---
+++ javaProject/HtmlCssProject/WebContent/day0611/popup.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+//웹브라우져 새창열때 가장간단(url,이름,스펙)
+function pop1() {
+	window.open('../image/popup3.jpg', 'Event',
+			'width=430,height=430,scrollbar=yes,status=no'); // url,name,spec
+
+}
+function pop2() {
+	window.open('http://m.naver.com', 'Naver',
+			'width=430,height=430,scrollbar=yes,status=no');
+}
